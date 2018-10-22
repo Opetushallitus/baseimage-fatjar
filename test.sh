ea8c9adc88edefa70f5e22c419d5ef842c675580
Ville Seppänen <ville.seppanen@gofore.com>
2018-10-22 13:16:19 +0300

    Update tests

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 # Strict mode
 set -eu
 
-# Tests
+echo "Test that required software is installed"
 apk --version
 aws --version
 java -version
 j2 --version
 cat /etc/alpine-release
 
-# Show directory sizes
-du -d 2 -h /
+echo "Test that baseimage has files expected by the application during run script"
+ls -la /root/jmx_prometheus_javaagent.jar
+ls -la /root/node_exporter
+
+echo "Largest directories:"
+du -d 3 -m /|sort -nr|head -n 20
